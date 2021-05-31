67a50d18ffd93d717113269c72c5c38085374947
rinku368368 <83747690+rinku368368@users.noreply.github.com>
2021-05-31 16:32:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ document.addEventListener("keypress",function(event){
 function makeSound(key){
 
         switch (key) {
-          case "w":
-              alert("bye")
+          case "w":  
             var tom1 = new Audio("tom-1.mp3");
             tom1.play();
             break;
